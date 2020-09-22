dac5f9eb5ea025b405fe1127c696d5073959b237
Josh Bumbalough <jbumb9000@gmail.com>
2020-09-21 23:50:18 -0500

    Added Assignment4
    Challenge3 and Prototype3

---
+++ Challenge3/Assets/Challenge 3/Scripts/ScoreManagerX.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ScoreManagerX : MonoBehaviour
+{
+    public static bool gameOver;
+    public static bool won;
+    public static int score;
+    public Text textbox;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameOver = false;
+        won = false;
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!gameOver)
+        {
+            textbox.text = "Score: " + score;
+        }
+
+        if(score >= 3)
+        {
+            won = true;
+            gameOver = true;
+        }
+
+        if(gameOver)
+        {
+            if(won)
+            {
+                textbox.text ="You win!\nPress R To Try Again!";
+            }
+            else
+            {
+                textbox.text = "You Lose!\nPress R To Try Again!";
+            }
+            if(Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+    }
+}
---
+++ Prototype3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    public float jumpForce;
+    public ForceMode forceMode;
+    public float gravityModifier;
+
+    public bool isOnGround = true;
+    public bool gameOver = false;
+
+    public Animator playerAnimator;
+
+    public ParticleSystem explosionParticle; 
+
+    public ParticleSystem dirtParticle;
+
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+    private AudioSource playerAudio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        //set reference to animator comp
+        playerAnimator = GetComponent<Animator>();
+
+        //start running animator on start
+        playerAnimator.SetFloat("Speed_f", 1.0f);
+
+        //set ref to audio source
+        playerAudio = GetComponent<AudioSource>();
+
+        forceMode = ForceMode.Impulse;
+
+        //Physics.gravity *= gravityModifier;
+
+        if(Physics.gravity.y > -10)
+        {
+            Physics.gravity *= gravityModifier;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
+        {
+            rb.AddForce(Vector3.up * jumpForce, forceMode);
+
+            isOnGround = false;
+
+            //set trigger to play jump animation
+            playerAnimator.SetTrigger("Jump_trig");
+
+            //stop playing dirt on jump
+            dirtParticle.Stop();
+
+            //play jump
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
+        }
+    }
+ 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+            //play dirt particle when the player hits the ground
+            dirtParticle.Play();
+        }
+        else if(collision.gameObject.CompareTag("Obstacle"))
+        {
+            Debug.Log("Game Over");
+            gameOver = true;
+
+            //play death animation
+            playerAnimator.SetBool("Death_b", true);
+            playerAnimator.SetInteger("DeathType_int", 1);
+
+            //play explosion particle
+            explosionParticle.Play();
+
+            playerAudio.PlayOneShot(crashSound, 1.0f);
+        }
+    }
+}
