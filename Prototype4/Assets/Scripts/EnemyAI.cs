71e1be655302c69253ea8ca3328c882d07ce36f9
Josh Bumbalough <jbumbalough@mail.bradley.edu>
2020-11-04 23:01:57 -0600

    Prototype 4

---
+++ Prototype4/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    private Rigidbody enemyRb;
+    public GameObject player;
+    public float speed = 3.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        //add force toward the direction from the player to the enemy
+
+        //vector for direction
+        Vector3 lookDirection = (player.transform.position - transform.position).normalized;
+
+        enemyRb.AddForce(lookDirection * speed);
+
+        if(transform.position.y < -10)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Prototype4/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+
+    public GameObject enemyPrefab;
+    private float spawnRange = 9;
+    public int enemyCount;
+    public static int waveNumber = 1;
+    public GameObject powerupPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        waveNumber = 1;
+        SpawnEnemyWave(waveNumber);
+        SpawnPowerup(1);
+    }
+
+    private Vector3 GenerateSpawnPosition()
+    {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+        return randomPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyCount = GameObject.FindGameObjectsWithTag("Enemy").Length;
+
+        if(enemyCount == 0)
+        {
+            waveNumber++;
+            SpawnEnemyWave(waveNumber);
+            SpawnPowerup(1);
+        }
+
+        if(waveNumber == 10)
+        {
+            GameManager.win = true;
+        }
+    }
+
+     private void SpawnPowerup(int powerupsToSpawn)
+    {
+        for(int i = 0; i < powerupsToSpawn; i++)
+        {
+             Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+        }
+    }
+
+    private void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for(int i = 0; i < enemiesToSpawn; i++)
+        {
+             Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
+    }
+}
